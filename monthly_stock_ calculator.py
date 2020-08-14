26c5cf5e188221f5dd7e9febf819102c0273a9bb
jeffery1128 <chunhotse3-c@my.cityu.edu.hk>
2020-08-15 00:21:18 +0800

    Added cost, recent price need fix
    Can calculate cost of buying stock monthly, but recent price is not fixed, since dataframe only have monthly data.

--- monthly_stock_ calculator.py
+++ monthly_stock_ calculator.py
@@ -5,7 +5,7 @@ Created on Tue Aug 11 15:37:44 2020
 @author: USER
 """
 
-#import pandas as pd
+import pandas as pd
 from yahoofinancials import YahooFinancials
 import datetime as dt
 
@@ -14,9 +14,16 @@ def take_ticker_data(ticker,month,quantity):
     day = month *30
     end_date = (dt.date.today()).strftime('%Y-%m-%d')
     start_date = (dt.date.today()-dt.timedelta(day)).strftime('%Y-%m-%d')
-    data = yahoo_financials.get_historical_price_data(start_date, end_date, "daily")
-    print(data['AAPL']['prices'])
+    data = yahoo_financials.get_historical_price_data(start_date, end_date, "monthly")
+    price = data[ticker]['prices']
+    df =  pd.DataFrame(price)
+    df.set_index('formatted_date', inplace = True)
+    cost = df['close'].sum() * quantity
+    #recent_price = df.loc[end_date]['close']
+    print(cost)
+    return 1
     
 
 
-take_ticker_data("AAPL",12,2)  
+result= take_ticker_data("AAPL",12,2)  
+print(result)
