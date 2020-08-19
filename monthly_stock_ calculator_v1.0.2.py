d5b244c903cb76977651ca6711140dbc0d20eb2a
jeffery1128 <chunhotse3-c@my.cityu.edu.hk>
2020-08-20 00:48:26 +0800

    function is done. Wait for firebase part

---
+++ monthly_stock_ calculator_v1.0.2.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 11 15:37:44 2020
+
+@author: USER
+"""
+
+import pandas as pd
+from yahoofinancials import YahooFinancials
+import datetime as dt
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("C:/Users/Chun Ho Tse/git/monthly_stock_calculator/monthly_stock_calculator/monthly-stock-calculator.json")
+firebase_admin.initialize_app(cred)
+
+def take_ticker_data(ticker,month,quantity):
+    yahoo_financials = YahooFinancials(ticker)
+    day = month *30
+    end_date = (dt.date.today()).strftime('%Y-%m-%d')
+    yesterday = (dt.date.today()-dt.timedelta(1)).strftime('%Y-%m-%d')
+    start_date = (dt.date.today()-dt.timedelta(day)).strftime('%Y-%m-%d')
+    month_data = yahoo_financials.get_historical_price_data(start_date, end_date, "monthly")
+    daily_data = yahoo_financials.get_historical_price_data(start_date, end_date, "daily")
+    df =  pd.DataFrame(month_data[ticker]['prices'])
+    daily_df = pd.DataFrame(daily_data[ticker]['prices'])
+    df.set_index('formatted_date', inplace = True)
+    daily_df.set_index('formatted_date', inplace = True)
+    cost = df['close'].sum() * quantity
+    recent_price = df.loc[df.last_valid_index()]['close']
+    total_quantity = month * quantity
+    now_asset = recent_price * total_quantity
+    average_price = cost / (total_quantity)
+    percentage = ((now_asset - cost)/cost)*100
+    print('Ticker : ',ticker)
+    print('Cost : ',cost)
+    print('Now Worth : ' , now_asset)
+    print('Average Stock Price : ' , average_price)
+    print('Recent Price : ' , recent_price)
+    print('Win/Loss Percentage : ' , percentage ,'%')
+    result= dict()
+    result['Ticker'] = ticker
+    result['Cost'] = cost
+    result['Now_Asset'] = now_asset
+    result['Average_stock_price'] = average_price
+    result['Recent_Price'] = recent_price
+    result['Percentage'] = percentage
+    return result
+    
+
+
+result= take_ticker_data("NFLX",12,2)  
+
